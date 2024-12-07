02d114316dd380448b2eccd5cbc6377b21c42d97
proliboon <proliboon@gmail.com>
2024-12-08 04:16:38 +0800

    added static class method for fetching current number of cart items

--- src/CartItem.java
+++ src/CartItem.java
@@ -2,6 +2,7 @@ public class CartItem {
     private final String name;
     private final double price;
     private final int quantity;
+    private static int cartItems = 0;
 
     public CartItem(String name, double price, int quantity) {
         this.name = name;
@@ -20,4 +21,6 @@ public class CartItem {
     public int getQuantity() {
         return quantity;
     }
+
+    public int getCartItems() {return cartItems;}
 }
